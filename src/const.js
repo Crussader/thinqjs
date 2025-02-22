d52ae80dd5890540d2bd19b1798868b211ceb086
Parikshit Rao <parikshitrao@Parikshits-MacBook-Pro.local>
2025-02-22 17:46:25 +0530

    Constants, updating npm package

---
+++ src/const.js
@@ -0,0 +1,11 @@
+export const API_KEY = '';
+export const CLIENT_PREFIX = '';
+
+export const deviceType = {
+    AIR_CONDITIONER: 'DEVICE_AIR_CONDITIONER',
+};
+
+export const property = {
+    READABLE: "r",
+    WRITABLE: "w",
+};
